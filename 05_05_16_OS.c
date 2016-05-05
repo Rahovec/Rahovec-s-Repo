dbb1ef845ac1435ee99a0c5f0df97acfcdf8b096
Konstantin Todorov <konstantin.todorov98@Gmail.com>
2016-05-05 11:04:01 +0000

    Update 05_05_16_OS.c

--- 05_05_16_OS.c
+++ 05_05_16_OS.c
@@ -6,10 +6,15 @@
 #include<stdio.h>
 #include <sys/stat.h>
 #include <unistd.h>
+#include <pwd.h>
 int main(){
     struct stat st;
+    struct passwd *pwd = getpwuid(st.st_uid);    
+
     stat("a.out", &st);
     printf("%d\n", st.st_size);
 
+    /*get
+    printf("%s\n", pwd->pw_name);*/ //tva trqq zima imeto na potrebitelq
 return 0;
 }
