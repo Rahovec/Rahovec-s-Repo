3486cf078a1d83996a861ee35f9bb172c7302588
Konstantin Todorov <konstantin.todorov98@Gmail.com>
2017-02-27 23:24:36 +0200

    Update CanModule.h

--- ArduinoDUE/CanModule.h
+++ ArduinoDUE/CanModule.h
@@ -1,30 +1,47 @@
 struct daily{
-	int maf; //can
-	int consumption; // can
+	float maf; //can
+	float consumption; // can
 	int rpm; //can
-	float obd_speed; //can
-	float gps_speed; //gps
+	float speed; //(obd + gps)/2
 };
 
 struct drag{
 	int rpm;
-	float obd_speed;
-	float gps_speed;
+	float speed;
 	float accel; //0-100 gps/obd speed avr
-/*can be float*/	int sixty_ft; //60ft gps + time
+	float sixty_ft; //60ft gps + time /*can be float*/	
+	float quarter; //quarter mile
 };
 
 struct race{
-	float obd_speed;
-	float gps_speed;
+	float speed;
 	float g_force; //9axis
 	int throttle_pos; //can
 	long lap_time //gps
 };
 
 struct drift{
-	float obd_speed;
-	float gps_speed;
+	float speed;
 	int turn_vector; //9 axis somehow ... not shure if int
 	int turn_rotation //9axis yaw pitch roll ...
+};
+/////////////////////////////////////////////////////
+struct daily{
+	int maf; //can
+	int consumption; // can
+	int rpm; //can
+	float obd_speed; //(obd + gps)/2
+};
+
+struct drag{
+	int rpm;
+	float obd_speed;
+};
+
+struct race{
+	float obd_speed;
+};
+
+struct drift{
+	float obd_speed;
 };
